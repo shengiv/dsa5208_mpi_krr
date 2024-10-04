7906f5194fa3e117a58ad7921334fd0cfac129d5
avellinwong01 <avellin.wong@gmail.com>
2024-10-05 05:15:04 +0800

    Debug conjugate gradient, add hyperparam tuning, README and summary

--- README.md
+++ README.md
@@ -1,1 +1,52 @@
-# dsa5208_mpi_krr
+# DSA5208 Project 1: MPI Kernel Ridge Regression
+
+---
+
+## Team Members
+
+| Name                 | Email Addess       | Student ID |
+|:--------------------:|:------------------:|:----------:|
+|Wong Zi Xin, Avellin  |e0580073@u.nus.edu  | A0225646B  |
+|Name            | Email | ID  |
+
+---
+
+# Deliverables 
+
+- In this submission folder, we have included a few files with remarks as follows:
+    - `distcomp.yaml`: Contains the consolidated package names and versions to install within an enclosed conda environment.
+    - `housing.tsv`: The provided dataset of California housing prices
+    - `README.md`: This current document providing instructions on how to run the MPI pipeline.
+    - `krr_mpi_script.py`: The MPI pipeline with Python binding, with which Kernel Ridge Regression will be implemented in distributed manner within MPI environment
+    - `summary.pdf`: Pdf file that documents the MPI implementation method, HyperParameter tuning details, and best RMSE result.
+
+---
+
+# Instructions to Run the MPI Pipeline
+
+## Step 1: Set up Conda Environment
+
+- Create a new conda environment called `distcomp` based on packages that are dumped into YAML file below.
+- Run below command in the terminal.
+
+```bash
+conda env create --file distcomp.yaml
+```
+
+## Step 2: Activate `distcomp` Conda Environment
+
+- Run below command in the terminal.
+
+```bash
+conda activate distcomp
+```
+
+## Step 3: Run MPI Pipeline with Python Binding
+
+- The number of participating processors can be adjusted accordingly by replacing the argument `8` below.
+- Please ensure to run below command at same directory level as `housing.tsv` file.
+- Run below command in the terminal. 
+
+```bash
+mpiexec -n 8 python krr_mpi_script.py
+```
--- krr_mpi_script.py
+++ krr_mpi_script.py
@@ -1,41 +1,9 @@
 from mpi4py import MPI
 import numpy as np
-from sklearn.preprocessing import MinMaxScaler, StandardScaler
+from sklearn.preprocessing import MinMaxScaler
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import mean_squared_error, r2_score
-
-comm = MPI.COMM_WORLD
-rank = comm.Get_rank()
-size = comm.Get_size()
-
-hyper_param_s = 1.5 # Smoothness parameter of Gaussian kernel provided by user
-hyper_param_lmbda = 0.2 #Regularization parameter of Gaussian kernel provided by user
-
-# Loading and standardizing data
-if rank == 0:
-    scaler = MinMaxScaler()
-    print("loading housing data")
-    data = np.genfromtxt('housing.tsv', delimiter='\t', skip_header=0)
-    X = data[:, :9]
-    y = data[:, 9]
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=2)
-    X_train = scaler.fit_transform(X_train)
-    X_test = scaler.transform(X_test)
-
-else:
-    X_train = None
-    X_test = None
-    y_train = None
-    y_test = None
-
-
-
-#All processes have all data points, might need to initialize array shape in all processes before broadcasting
-X_train = comm.bcast(X_train, root=0)
-y_train = comm.bcast(y_train, root=0)
-X_test  = comm.bcast(X_test,  root=0)
-y_test  = comm.bcast(y_test,  root=0)
-
+from sklearn.metrics import root_mean_squared_error
+from tqdm import tqdm
 
 #Distributed computation of matrix A
 def local_conjugate_gradient(local_A, local_y, local_alpha, local_row_offset, global_p, comm, threshold):
@@ -51,7 +19,8 @@ def local_conjugate_gradient(local_A, local_y, local_alpha, local_row_offset, gl
         #Matrix multiplication
         comm.Allgatherv(local_p, [global_p, local_sizes, offsets, MPI.DOUBLE])
         w = local_A @ global_p
-        s = squared_error/(np.dot(local_p,w))
+        # s = squared_error/(np.dot(local_p,w))
+        s = squared_error/comm.allreduce(np.dot(local_p, w), op=MPI.SUM)
         local_alpha += s*local_p
         local_r -= s*w
         new_squared_error = comm.allreduce(np.dot(local_r, local_r), op=MPI.SUM)
@@ -62,7 +31,7 @@ def local_conjugate_gradient(local_A, local_y, local_alpha, local_row_offset, gl
         if (curr_iter > max_iter):
             break
         #if (curr_iter%50 == 0 and rank == 0):
-        print("iter: " + str(curr_iter) + "squared error: " + str(squared_error))
+        # print("iter: " + str(curr_iter) + "squared error: " + str(squared_error))
     return local_alpha
 
 def gaussian_kernel_vectorised(x, xi, s):
@@ -73,50 +42,118 @@ def gaussian_kernel_vectorised(x, xi, s):
 # def gaussian_kernel(x, xi, s):
 #     return np.exp((-np.linalg.norm(x - xi) ** 2)/(2 * s ** 2))
 
-num_total_samples = len(X_train)
-row_offset_per_process = num_total_samples//size
-num_of_rows_in_last_process = row_offset_per_process + (num_total_samples%size)
-number_of_rows_in_process = num_of_rows_in_last_process if rank == size - 1 else row_offset_per_process
+def main(): 
+    comm = MPI.COMM_WORLD
+    rank = comm.Get_rank()
+    size = comm.Get_size()
 
-local_A = np.zeros((number_of_rows_in_process, num_total_samples))
-local_y = np.zeros(number_of_rows_in_process)
-local_alpha = np.zeros(number_of_rows_in_process)
-local_row_offset = rank*row_offset_per_process
-global_p = np.zeros(num_total_samples)
+    # Loading and standardizing data
+    if rank == 0:
+        scaler = MinMaxScaler()
+        print("loading housing data")
+        data = np.genfromtxt('housing.tsv', delimiter='\t', skip_header=0)
+        X = data[:, :9]
+        y = data[:, 9]
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=2)
+        X_train = scaler.fit_transform(X_train)
+        X_test = scaler.transform(X_test)
+
+    else:
+        X_train = None
+        X_test = None
+        y_train = None
+        y_test = None
+
+    #All processes have all data points, might need to initialize array shape in all processes before broadcasting
+    X_train = comm.bcast(X_train, root=0)
+    y_train = comm.bcast(y_train, root=0)
+    X_test  = comm.bcast(X_test,  root=0)
+    y_test  = comm.bcast(y_test,  root=0)
+
+    
+    # hyper_param_s = 1.5 # Smoothness parameter of Gaussian kernel provided by user
+    # hyper_param_lmbda = 0.8 #Regularization parameter of Gaussian kernel provided by user
+    # hyper_param_lmbda = 1.40e-2
+    # hyper_param_s = 2.5
+
+    ## Coarse-Grained HyperParameter Tuning Range
+    ## Best s 1.00e-01, Best lambda 1.00e-01, Best RMSE_test 59770.2649...
+    # s_list     = [1e-7*(10**i) for i in range(1,  7)]  # s is a smoothness parameter for Gaussian Kernel. 
+    # lmbda_list     = [1e-6*(10**i) for i in range(1,  9)]  # Lmbda is a Regularization parameter for Gaussian Kernel
+
+    ## Fine-Grained HyperParameter Tuning Range
+    ## Best s 1.20e-01, Best lambda 1.10e-01, Best RMSE_test 56805.2882...
+    s_list     = [3e-2   + 1e-2*i for i in range(10)]  # s is a smoothness parameter for Gaussian Kernel. 
+    lmbda_list = [1.1e-1 + 1e-2*i for i in range(10)]  # Lmbda is a Regularization parameter for Gaussian Kernel
+
+    best_s  = 1.0
+    best_lmbda  = 1.0
+    best_rmse_test   = np.inf
+    combinations= [(s, lmbda) for s in s_list for lmbda in lmbda_list]
+
+    for s, lmbda in tqdm(combinations, desc="Gaussian Kernel Tuning...", ascii=False, ncols=70):
+        num_total_samples = len(X_train)
+        row_offset_per_process = num_total_samples//size
+        num_of_rows_in_last_process = row_offset_per_process + (num_total_samples%size)
+        number_of_rows_in_process = num_of_rows_in_last_process if rank == size - 1 else row_offset_per_process
+
+        local_A = np.zeros((number_of_rows_in_process, num_total_samples))
+        local_y = np.zeros(number_of_rows_in_process)
+        local_alpha = np.zeros(number_of_rows_in_process)
+        local_row_offset = rank*row_offset_per_process
+        global_p = np.zeros(num_total_samples)
 
 
-#Local kernel computation
-for i in range(len(local_A)):
-    row_num = rank*row_offset_per_process + i
-    local_y[i] = y_train[row_num]
+        #Local kernel computation
+        for i in range(len(local_A)):
+            row_num = rank*row_offset_per_process + i
+            local_y[i] = y_train[row_num]
 
-    local_A[i] = (gaussian_kernel_vectorised(X_train[row_num], X_train, hyper_param_s))
-    local_A[i, row_num] += hyper_param_lmbda
+            # print("row num", X_train[row_num].shape)
+            # print(X_train.shape)
+            local_A[i] = (gaussian_kernel_vectorised(X_train[row_num], X_train, s))
+            local_A[i, row_num] += lmbda
+
+        local_alpha = local_conjugate_gradient(local_A, local_y, local_alpha, local_row_offset, global_p, comm, 1e-6)
+
+        # Prediction and Test
+        local_y_pred = np.zeros(len(y_test))
+        if rank == 0:
+            global_y_pred = np.zeros(len(y_test))
+        else:
+            global_y_pred = None
+
+        for i in range(len(y_test)):
+            # for j in range(number_of_rows_in_process):
+                # row_num = rank*row_offset_per_process + j
+                start = rank*row_offset_per_process
+                end = start + number_of_rows_in_process
+                # print(X_test[i].shape)
+                # print(X_train[row_num].shape)
+                # local_y_pred[i] += local_alpha[j]*gaussian_kernel(X_test[i], X_train[row_num], hyper_param_s)
+                local_y_pred[i] = np.dot(local_alpha, gaussian_kernel_vectorised(X_test[i], X_train[start:end], s))
+                # print(local_y_pred[i])
+                # print("shape", local_y_pred.shape)
+                # print((local_alpha[j]*gaussian_kernel(X_test[i], X_train[row_num], hyper_param_s)).shape)
+                # print(np.sum(local_alpha[j]*gaussian_kernel_vectorised(X_test[i], X_train, hyper_param_s)))
 
 
+        comm.Reduce([local_y_pred, MPI.DOUBLE], [global_y_pred, MPI.DOUBLE], op=MPI.SUM, root=0)
 
-local_alpha = local_conjugate_gradient(local_A, local_y, local_alpha, local_row_offset, global_p, comm, 1e-6)
+        if rank == 0:
+            rmse = root_mean_squared_error(y_test, global_y_pred)
+            print("Mean squared error of test set = " + str(rmse))
+            if rmse < best_rmse_test:
+                best_s = s
+                best_lmbda = lmbda
+                best_rmse_test  = rmse
+    
+    if rank == 0: print(f"\nGaussian Kernel ==> Best s {best_s:.2e}, Best lambda {best_lmbda:.2e}, Best RMSE_test {best_rmse_test:.4f}...\n")
 
-#Test
-# local_train_se = 0
-# local_test_se = 0
+        
 
-# local_y_pred = np.zeros(len(y_test))
-# if rank == 0:
-#     global_y_pred = np.zeros(len(y_test))
-# else:
-#     global_y_pred = None
+main()
 
-# for i in range(len(y_test)):
-#     for j in range(number_of_rows_in_process):
-#         row_num = rank*row_offset_per_process + j
-#         local_y_pred[i] += local_alpha[j]*gaussian_kernel(X_test[i], X_train[row_num], hyper_param_s)
-
-# comm.Reduce([local_y_pred, MPI.DOUBLE], [global_y_pred, MPI.DOUBLE], op=MPI.SUM, root=0)
-
-# if rank == 0:
-#     mse = mean_squared_error(y_test, global_y_pred)
-#     print("Mean squared error of test set = " + str(mse))
 
 
 
